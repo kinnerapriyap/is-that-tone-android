0e1095060911237b584220e8bd54f455d66914a2
kinnerapriyap <kinnerapriyap@gmail.com>
2021-02-28 15:52:50 +0900

    Add default values in GameRoom

--- app/src/main/java/com/kinnerapriyap/sugar/data/GameRoom.kt
+++ app/src/main/java/com/kinnerapriyap/sugar/data/GameRoom.kt
@@ -2,9 +2,9 @@ package com.kinnerapriyap.sugar.data
 
 data class GameRoom(
     val roundsInfo: Map<Int, Map<String, Int>>? = null,
-    val activeRound: Int? = null,
+    val activeRound: Int = 1,
     val players: Map<String?, String?>? = null,
     val currentPlayer: String? = null,
     @field:JvmField // use this annotation if your Boolean field is prefixed with 'is'
-    val isStarted: Boolean? = null
+    val isStarted: Boolean = false
 )
