68a16977efb8262e16e281e25ace1effa1fce120
kinnerapriyap <kinnerapriyap@gmail.com>
2021-02-28 13:07:00 +0900

    Add roomName and userName field to HomeScreen

--- app/src/main/java/com/kinnerapriyap/sugar/MainViewModel.kt
+++ app/src/main/java/com/kinnerapriyap/sugar/MainViewModel.kt
@@ -1,6 +1,20 @@
 package com.kinnerapriyap.sugar
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    private val _roomName = MutableLiveData("")
+    val roomName: LiveData<String> = _roomName
+    fun onRoomNameChanged(newName: String) {
+        _roomName.value = newName
+    }
+
+    private val _userName = MutableLiveData("")
+    val userName: LiveData<String> = _userName
+    fun onUserNameChanged(newName: String) {
+        _userName.value = newName
+    }
 }
