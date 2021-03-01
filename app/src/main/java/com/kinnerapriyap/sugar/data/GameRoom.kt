339aca76458c7dbc8b10b944f4f8e156ed50e9bf
kinnerapriyap <kinnerapriyap@gmail.com>
2021-03-01 21:34:46 +0900

    Make Player List instead of map in GameRoom and give default values

--- app/src/main/java/com/kinnerapriyap/sugar/data/GameRoom.kt
+++ app/src/main/java/com/kinnerapriyap/sugar/data/GameRoom.kt
@@ -2,12 +2,14 @@ package com.kinnerapriyap.sugar.data
 
 data class GameRoom(
     // (roundAsString to (uid to answer))
-    val roundsInfo: Map<String, Map<String, String?>>? = null,
-    val activeRound: Int? = null,
+    val roundsInfo: Map<String, Map<String, String?>> = emptyMap(),
+    val activeRound: Int = 0,
     // uid to userName
-    val players: Map<String?, String?>? = null,
-    val activePlayer: String? = null,
+    val players: List<Player> = emptyList(),
     @field:JvmField // use this annotation if your Boolean field is prefixed with 'is'
     val isStarted: Boolean = false,
-    val wordCards: List<WordCard>? = null
-)
+    val wordCards: List<WordCard> = emptyList()
+) {
+    fun isActivePlayer(uid: String) =
+        players.getOrNull(if (activeRound == 0) 0 else activeRound - 1)?.uid == uid
+}
---
+++ app/src/main/java/com/kinnerapriyap/sugar/data/Player.kt
@@ -0,0 +1,6 @@
+package com.kinnerapriyap.sugar.data
+
+data class Player(
+    val uid: String? = null,
+    val name: String? = null
+)
